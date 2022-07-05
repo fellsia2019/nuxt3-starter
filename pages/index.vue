3420c4eeedf9e751b9a10fc2caf5496d41c7d981
ar00tem <mushket.1998@mail.ru>
2022-07-05 11:32:23 +0300

    tailwind config custom

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="index-page text-3xl font-bold underline">
-        <h1 class="title text-blue-700">index</h1>
+        <h1 class="title text-blue-100 hover:text-blue-700 px-3 py-2">index</h1>
     </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    content: [
+        './components/**/*.{js,vue,ts}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './plugins/**/*.{js,ts}',
+    ],
+    theme: {
+        colors: {
+            blue: {
+                100: '#1fb6ff',
+                700: 'blue',
+            },
+        },
+        spacing: {
+            1: '8px',
+            //
+            2: '15px',
+            3: '18px',
+            4: '20px',
+            5: '25px',
+            6: '35px',
+            //
+            7: '48px',
+        },
+        extend: {},
+    },
+    plugins: [],
 };
